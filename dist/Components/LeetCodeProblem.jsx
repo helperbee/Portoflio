b8b4a3117a86c533a940e5b1d794328ee89599cd
M <amfrombr@gmail.com>
2023-08-23 19:31:18 -0400

    Logos added for languages, but it's kind of ugly. Adjust later

--- dist/Components/LeetCodeProblem.jsx
+++ dist/Components/LeetCodeProblem.jsx
@@ -6,7 +6,10 @@ import React from 'react';
 
 let LeetCodeProblem = ({problem}) => {
     return (
+        <>
         <h3>{problem.title}</h3>
+        <img src={`https://img.shields.io/badge/${problem.lang}-%23007ACC.svg?style=for-the-badge&logo=${problem.lang}`}/>
+        </>
     );
 }
 
