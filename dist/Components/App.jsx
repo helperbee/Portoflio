2cbc63eafc17342b5e0a78fe704670ba25311d13
M <amfrombr@gmail.com>
2023-08-23 21:47:47 -0400

    added typed.js to mess around with. kind of know how i'll design the site from here on.

--- dist/Components/App.jsx
+++ dist/Components/App.jsx
@@ -1,13 +1,54 @@
 import React, { useState, useEffect } from 'react';
+import Typed from 'typed.js';
+import { Container, Box, createTheme, ThemeProvider } from '@mui/material';
 import LeetCode from './LeetCode.jsx';
 
-
+const theme = createTheme({
+  palette: {
+    mode: 'dark', // Set the theme mode to dark
+    primary: {
+      main: '#90caf9', // Adjust primary color for dark mode
+    },
+    secondary: {
+      main: '#f48fb1', // Adjust secondary color for dark mode
+    },
+    background: {
+      default: '#121212', // Adjust background color for dark mode
+      paper: '#1e1e1e', // Adjust paper color for dark mode
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, Arial, sans-serif',
+  },
+});
 export default function App() {
-  
+  const el = React.useRef(null);
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["I'm Mike, welcome to my development sampler."],
+      startDelay: 300,
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 100
+    });
+    return () => {
+      typed.destroy();
+    };
+  }, []);
   return (
-    <>
-      <h1>Hello there</h1>
-      <LeetCode />
-    </>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="md">
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            minHeight="100vh"
+          >
+          <h1 ref={el}></h1>
+          <LeetCode />
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
--- dist/Components/LeetCodeProblem.jsx
+++ dist/Components/LeetCodeProblem.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 
 let LeetCodeProblem = ({problem}) => {
-    return (
+    return (//https://github.com/Ileriayo/markdown-badges
         <>
         <h3>{problem.title}</h3>
         <img src={`https://img.shields.io/badge/${problem.lang}-%23007ACC.svg?style=for-the-badge&logo=${problem.lang}`}/>
